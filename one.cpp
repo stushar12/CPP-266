5626c19f8fcd3c1572bcd47b7cc9f958f89f3367
Tushar Saxena <stushar345@gmail.com>
2021-04-22 20:12:14 +0530

    Add files via upload

---
+++ one.cpp
@@ -0,0 +1,39 @@
+ struct compare 
+ {
+       bool operator()(Node* a, Node* b)
+    {
+        return a->data > b->data;
+    }
+};
+
+ Node* mergeKSortedLists(Node* arr[], int k)
+{
+    Node *head = NULL, *last;
+    priority_queue<Node*, vector<Node*>, compare> pq;
+
+    for (int i = 0; i < k; i++)
+        if (arr[i] != NULL)
+            pq.push(arr[i]);
+
+    while (!pq.empty()) 
+    {
+        Node* top = pq.top();
+        pq.pop();
+
+        if (top->next != NULL)
+            pq.push(top->next);
+
+        if (head == NULL) 
+        {
+            head = top;
+            last = top;
+        }
+
+        else 
+        {
+            last->next = top;
+            last = top;
+        }
+    }
+    return head;
+}
